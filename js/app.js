81f095f1e440787e68b4186c52e59cf64a392e54
Li-Xin <mzydj@139.com>
2017-12-28 14:28:24 +0800

    使用组织库：添加更多的猫

--- js/app.js
+++ js/app.js
@@ -1,25 +1,74 @@
+let model = {
+	moves: [0, 0, 0, 0, 0],
+	cat: 0,
+	catName: ['cat1', 'cat2', 'cat3', 'cat4', 'cat5'],
+	catImgList: ['img/22252709_010df3379e_z.jpg',
+		'img/434164568_fea0ad4013_z.jpg',
+		'img/1413379559_412a540d29_z.jpg',
+		'img/4154543904_6e2428c421_z.jpg',
+		'img/9648464288_2516b35537_z.jpg'
+	],
+	nameList: [
+		['test1', 'cat2'],
+		['cat21'],
+		['cat31'],
+		['cat41'],
+		['cat51']
+	]
+}
+
+class Cat {
+	constructor(data) {
+		this.clickCount = ko.observable(data.clickCount);
+		this.name = ko.observable(data.name);
+		this.imgSrc = ko.observable(data.imgSrc);
+		this.nameList = ko.observableArray(data.nameList);
+		this.grade = ko.dependentObservable(() => {
+			switch (true) {
+				case this.clickCount() < 10:
+					return 'Newborn';
+					break;
+				case this.clickCount() < 100:
+					return 'Infant';
+					break;
+				case this.clickCount() < 200:
+					return 'Teen';
+			}
+		});
+	}
+}
+let that;
 class ViewModel {
 	constructor() {
-		this.clickCount = ko.observable(0);
-		this.name = ko.observable('Tabby');
-		this.imgSrc = ko.observable('img/22252709_010df3379e_z.jpg');
-		this.imgAttribution = ko.observable('https?fadfdkwef');
-		this.grade = ko.dependentObservable(() => {
-			switch(true){
-				case this.clickCount() < 10:
-				return 'Newborn';
-				break;
-				case this.clickCount() <100:
-				return 'Infant';
-				break;
-				case this.clickCount() <200:
-				return 'Teen';
-			}
-		})
+		that = this;
+		this.catList = ko.observableArray([]);
+		this.cat = ko.observable(model.cat)
+		model.catName.forEach((name, inedx) => {
+			this.catList.push(new Cat({
+				clickCount: model.moves[inedx],
+				name: name,
+				imgSrc: model.catImgList[inedx],
+				nameList: model.nameList[inedx]
+			}));
+		});
+
+		$('.catlist').on('click', 'li', this.chooseCat);
+
+		this.currentCat = ko.dependentObservable(() =>{
+			return this.catList()[this.cat()];
+		});
 	}
 
 	incrementCounter() {
+		// console.log(this)
 		this.clickCount(this.clickCount() + 1);
+	}
+
+	chooseCat() {
+		console.log(this)
+		// console.log(that)
+		// console.log($.inArray(this.name(), model.catName))
+		that.cat($.inArray(this.name(), model.catName));
 	}
 }
 
